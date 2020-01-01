3069a3fa2f7e6756ed56032e05008658e5d5f219
Seven Lju <dna2gle@gmail.com>
2020-01-02 01:11:39 +0800

    [bk] ws api for file browsing

---
+++ server/component/storage.js
@@ -0,0 +1,135 @@
+/**
+ * IStorage {
+ *    lstat,
+ *    mkdir,
+ *    rmdir,
+ *    readdir,
+ *    rename,
+ *    create,
+ *    unlink,
+ *    open,
+ *    close,
+ *    read,
+ *    write
+ * }
+ */
+
+const i_fs = require('fs');
+const i_path = require('path');
+
+const helper = {
+   fs: {
+      mkdir: async (path) => {
+         return new Promise((r, e) => {
+            i_fs.mkdir(path, (err) => {
+               if (err) return e(err);
+               r();
+            });
+         });
+      },
+      rmdir: async (path) => {
+         return new Promise((r, e) => {
+            i_fs.rmdir(path, (err) => {
+               if (err) return e(err);
+               r();
+            });
+         });
+      },
+      lstat: async (path) => {
+         return new Promise((r, e) => {
+            i_fs.lstat(path, (err, stat) => {
+               if (err) return e(err);
+               // TODO: transform to unified stat
+               r({
+                  mtime: stat.mtimeMs,
+                  atime: stat.atimeMs,
+                  ctime: stat.ctimeMs,
+                  isDirectory: stat.isDirectory(),
+                  isFile: stat.isFile(),
+               });
+            });
+         });
+      },
+      open: async (path, flags) => {
+         return new Promise((r, e) => {
+            i_fs.open(path, flags, (err, fd) => {
+               if (err) return e(err);
+               r(fd);
+            });
+         });
+      },
+      close: async (fd) => {
+         return new Promise((r, e) => {
+            i_fs.close(fd, (err) => {
+               if (err) return e(err);
+               r();
+            });
+         });
+      },
+      unlink: async (path) => {
+         return new Promise((r, e) => {
+            i_fs.unlink(path, (err) => {
+               if (err) return e(err);
+               r();
+            });
+         });
+      },
+      readdir: async (path) => {
+         return new Promise((r, e) => {
+            i_fs.readdir(path, (err, files) => {
+               if (err) return e(err);
+               r(files);
+            });
+         });
+      },
+      rename: async (path, newpath) => {
+         return new Promise((r, e) => {
+            i_fs.rename(path, newpath, (err) => {
+               if (err) return e(err);
+               r();
+            });
+         });
+      }
+   }, // fs
+};
+
+class LocalFilesystemStorage {
+   async mkdir (path) {
+      await helper.fs.mkdir(path);
+   }
+
+   async rmdir (path) {
+      await helper.fs.rmdir(path);
+   }
+
+   async readdir (path) {
+      let files = await helper.fs.readdir(path);
+      files = files.map((x) => ({ path: x }));
+      for (let i = 0, n = files.length; i < n; i++) {
+         let fileObj = files[i];
+         let filename = i_path.join(path, fileObj.path);
+         let stat = await helper.fs.lstat(filename);
+         if (stat.isDirectory) {
+            files[i].path += '/';
+         }
+      }
+      return files;
+   }
+
+   async create (path) {
+      let fd = await helper.fs.open(path, 'w+');
+      await helper.fs.close(fd);
+   }
+
+   async unlink (path) {
+      await helper.fs.unlink(path);
+   }
+
+   async move (path, newPath) {
+      await helper.fs.rename(path, newPath);
+   }
+}
+
+module.exports = {
+   LocalFilesystemStorage,
+};
---
+++ server/wsapi/file_browser.js
@@ -0,0 +1,88 @@
+const i_path = require('path');
+const i_storage = require('../component/storage');
+
+const system = {
+   baseDir: i_path.resolve(process.env.EDIENILNO_FS_STORAGE || '/tmp'),
+   storage: new i_storage.LocalFilesystemStorage(),
+};
+
+function validate_path(path) {
+   let parts = path.split(i_path.sep);
+   return parts.filter((x) => x !== '.' && x !== '..').join(i_path.sep);
+}
+
+const api = {
+   initialize: () => {},
+   process: (ws, m, env) => {
+      if (!m.cmd || !m.id || !m.path) return false;
+      let username = env.username;
+      if (!username) return false;
+      let base = i_path.join(system.baseDir, username);
+      let filename = validate_path(i_path.join(base, m.path));
+      let obj = { id: m.id };
+      switch(m.cmd) {
+         case 'fileBrowser.list':
+            system.storage.readdir(filename).then((file_list) => {
+               obj.list = file_list;
+               ws.send(JSON.stringify(obj));
+            }, (err) => {
+               console.log(err);
+               obj.error = 'failed';
+               obj.code = 1;
+               ws.send(JSON.stringify(obj));
+            });
+            break;
+         case 'fileBrowser.mkdir':
+            system.storage.mkdir(filename).then(() => {
+               ws.send(JSON.stringify(obj));
+            }, () => {
+               obj.error = 'failed';
+               obj.code = 1;
+               ws.send(JSON.stringify(obj));
+            });
+            break;
+         case 'fileBrowser.rmdir':
+            system.storage.rmdir(filename).then(() => {
+               ws.send(JSON.stringify(obj));
+            }, () => {
+               obj.error = 'failed';
+               obj.code = 1;
+               ws.send(JSON.stringify(obj));
+            });
+            break;
+         case 'fileBrowser.create':
+            system.storage.create(filename).then(() => {
+               ws.send(JSON.stringify(obj));
+            }, () => {
+               obj.error = 'failed';
+               obj.code = 1;
+               ws.send(JSON.stringify(obj));
+            });
+            break;
+         case 'fileBrowser.delete':
+            system.storage.unlink(filename).then(() => {
+               ws.send(JSON.stringify(obj));
+            }, () => {
+               obj.error = 'failed';
+               obj.code = 1;
+               ws.send(JSON.stringify(obj));
+            });
+            break;
+         case 'fileBrowser.move':
+            if (!m.newpath) return false;
+            let newFilename = validate_path(i_path.join(base, m.newpath));
+            system.storage.move(filename, newFilename).then(() => {
+               ws.send(JSON.stringify(obj));
+            }, () => {
+               obj.error = 'failed';
+               obj.code = 1;
+               ws.send(JSON.stringify(obj));
+            });
+            break;
+         default:
+            return false;
+      }
+      return true;
+   },
+};
+module.exports = api;
